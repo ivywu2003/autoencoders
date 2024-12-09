b1034b874f7befce76de0cd286ec44c806ab0c59
Frank <fle@mit.edu>
2024-12-09 18:01:37 -0500

    histograms

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -7,14 +7,13 @@ import torch
 import torchvision
 from torchvision import transforms
 import skimage.metrics
-import torchmetrics
 import matplotlib.pyplot as plt
 from mnist_mae_vit.mae_vit import MaskedAutoencoderViTForMNIST
 from mnist_mae_vit.dae import ConvDenoiser
-from cifar10_task.cifar10_models import CIFAR10Autoencoder, CustomCIFAR10MaskedAutoencoder
+from cifar10_task.cifar10_models import CIFAR10DenoisingAutoencoder, CustomCIFAR10MaskedAutoencoder
 
 
-def load_mnist():
+def load_mnist_for_mae():
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.5,), (0.5,))
@@ -24,10 +23,17 @@ def load_mnist():
     
     return testloader
 
-def load_cifar10():
+def load_mnist_for_dae():
+    transform = transforms.ToTensor()
+    testset = torchvision.datasets.MNIST('./data', download=True, train=False, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, shuffle=False)
+    
+    return testloader
+
+def load_cifar10_for_mae():
     transform = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2470, 0.2435, 0.2616])
+        transforms.Normalize(0.5, 0.5)
     ])
 
     testset = torchvision.datasets.CIFAR10(
@@ -36,6 +42,22 @@ def load_cifar10():
         testset, shuffle=False)
     
     return testloader
+
+def load_cifar10_for_dae():
+    transform = transforms.ToTensor()
+
+    testset = torchvision.datasets.CIFAR10(
+        root='./data', train=False, download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(
+        testset, shuffle=False)
+    
+    return testloader
+
+def denormalize(x):
+    """Denormalize images"""
+    mean = torch.tensor([0.5])
+    std = torch.tensor([0.5])
+    return x * std + mean
 
 def load_mnist_mae():
     mae_model = MaskedAutoencoderViTForMNIST(
@@ -60,7 +82,7 @@ def load_mnist_dae():
     return dae_model
 
 def load_cifar10_dae():
-    dae_model = CIFAR10Autoencoder()
+    dae_model = CIFAR10DenoisingAutoencoder()
     dae_model.load_state_dict(torch.load('cifar10_task/cifar10_dae_weights_20_epochs.pth', weights_only=True, map_location=torch.device('cpu'))) 
     dae_model.eval()
     return dae_model
@@ -80,12 +102,29 @@ def generate_mae_psnr_plot(image_loader, model, save_path, mask_ratio=0.75):
 
     with torch.no_grad():
         for image, _ in image_loader:
-            loss, pred, mask = model(image.float(), mask_ratio=mask_ratio)
-            reconstructed = model.unpatchify(pred)
+            # MNIST VERSION
+            # _, pred, mask = model(image.float(), mask_ratio=mask_ratio)
+            # mask = mask.unsqueeze(-1).repeat(1, 1, model.patch_embed.patch_size[0]**2)  # (N, H*W, p*p*3)
+            # mask = model.unpatchify(mask)
+
+            # reconstructed = model.unpatchify(pred)
+            # reconstructed = ((1-mask)*image) + (mask*reconstructed)
+
+            # CIFAR10 VERSION
+            reconstructed, mask = model(image.float(), mask_ratio=mask_ratio)
+            reconstructed = reconstructed * mask + image * (1 - mask)
+            image = denormalize(image)
+            reconstructed = denormalize(reconstructed)
 
             # renormalize
             image = (image - torch.min(image))/(torch.max(image) - torch.min(image))
             reconstructed = (reconstructed - torch.min(reconstructed))/(torch.max(reconstructed) - torch.min(reconstructed))
+
+            # plt.subplot(1, 2, 1)
+            # plt.imshow(image.squeeze(0).permute(1, 2, 0).numpy())
+            # plt.subplot(1, 2, 2)
+            # plt.imshow(reconstructed.squeeze(0).permute(1, 2, 0).numpy())
+            # plt.show()
             psnr = single_image_psnr(image, reconstructed)
             psnrs.append(psnr)
 
@@ -107,11 +146,30 @@ def generate_dae_psnr_plot(image_loader, model, save_path):
 
     with torch.no_grad():
         for image, _ in image_loader:
-            reconstructed, _ = model(image.float())
+            # MNIST VERSION
+            # noise_factor = 0.5
+            # noisy_imgs = image + noise_factor * torch.randn(image.shape)
+            # noisy_imgs = np.clip(noisy_imgs, 0., 1.)
+            # reconstructed, _ = model(noisy_imgs)
+            
+            # CIFAR10 VERSION
+            noise = torch.randn_like(image) * 0.1
+            noisy_images = image + noise
+            noisy_images = torch.clamp(noisy_images, 0., 1.)
+            reconstructed = model(image.float())
+            image = denormalize(image)
+            reconstructed = denormalize(reconstructed)
 
             # renormalize
             image = (image - torch.min(image))/(torch.max(image) - torch.min(image))
             reconstructed = (reconstructed - torch.min(reconstructed))/(torch.max(reconstructed) - torch.min(reconstructed))
+
+            # plt.subplot(1, 2, 1)
+            # plt.imshow(image.squeeze(0).permute(1, 2, 0).numpy())
+            # plt.subplot(1, 2, 2)
+            # plt.imshow(reconstructed.squeeze(0).permute(1, 2, 0).numpy())
+            # plt.show()
+
             psnr = single_image_psnr(image, reconstructed)
             psnrs.append(psnr)
 
@@ -130,14 +188,7 @@ def generate_dae_psnr_plot(image_loader, model, save_path):
 
 
 def structural_similarity(true_images, generated_images, greyscale=False):
-    print(true_images[0].shape)
-    print(generated_images[0].shape)
-
-    if greyscale:
-        channel_axis = 0
-    else:
-        channel_axis = 2
-    ssim = [skimage.metrics.structural_similarity(true_image, generated_image, channel_axis=channel_axis, data_range=1) for true_image, generated_image in zip(true_images, generated_images)]
+    ssim = [skimage.metrics.structural_similarity(true_image, generated_image, channel_axis=0, data_range=1) for true_image, generated_image in zip(true_images, generated_images)]
     return ssim
 
 def generate_mae_ssim_plot(image_loader, model, save_path, greyscale, mask_ratio=0.75):
@@ -146,12 +197,30 @@ def generate_mae_ssim_plot(image_loader, model, save_path, greyscale, mask_ratio
 
     with torch.no_grad():
         for image, _ in image_loader:
-            loss, pred, mask = model(image.float(), mask_ratio=mask_ratio)
-            reconstructed = model.unpatchify(pred)
+            # MNIST VERSION
+            # _, pred, mask = model(image.float(), mask_ratio=mask_ratio)
+            # mask = mask.unsqueeze(-1).repeat(1, 1, model.patch_embed.patch_size[0]**2)  # (N, H*W, p*p*3)
+            # mask = model.unpatchify(mask)
+
+            # reconstructed = model.unpatchify(pred)
+            # reconstructed = ((1-mask)*image) + (mask*reconstructed)
+
+            # CIFAR10 VERSION
+            reconstructed, mask = model(image.float(), mask_ratio=mask_ratio)
+            reconstructed = reconstructed * mask + image * (1 - mask)
+            image = denormalize(image)
+            reconstructed = denormalize(reconstructed)
 
             # renormalize
             image = (image - torch.min(image))/(torch.max(image) - torch.min(image))
             reconstructed = (reconstructed - torch.min(reconstructed))/(torch.max(reconstructed) - torch.min(reconstructed))
+
+            # plt.subplot(1, 2, 1)
+            # plt.imshow(image.squeeze(0).squeeze(0).numpy(), cmap='gray')
+            # plt.subplot(1, 2, 2)
+            # plt.imshow(reconstructed.squeeze(0).squeeze(0).numpy(), cmap='gray')
+            # plt.show()
+            # break
 
             true_images.append(image.squeeze(0).numpy())
             generated_images.append(reconstructed.squeeze(0).numpy())
@@ -177,7 +246,26 @@ def generate_dae_ssim_plot(image_loader, model, save_path, greyscale):
 
     with torch.no_grad():
         for image, _ in image_loader:
-            reconstructed, _ = model(image.float())
+            # MNIST VERSION
+            # noise_factor = 0.5
+            # noisy_imgs = image + noise_factor * torch.randn(image.shape)
+            # noisy_imgs = np.clip(noisy_imgs, 0., 1.)
+            # reconstructed, _ = model(noisy_imgs)
+            
+            # CIFAR10 VERSION
+            noise = torch.randn_like(image) * 0.1
+            noisy_images = image + noise
+            noisy_images = torch.clamp(noisy_images, 0., 1.)
+            reconstructed = model(image.float())
+            image = denormalize(image)
+            reconstructed = denormalize(reconstructed)
+
+            # plt.subplot(1, 2, 1)
+            # plt.imshow(image.squeeze(0).squeeze(0).numpy(), cmap='gray')
+            # plt.subplot(1, 2, 2)
+            # plt.imshow(reconstructed.squeeze(0).squeeze(0).numpy(), cmap='gray')
+            # plt.show()
+            # break
 
             # renormalize
             image = (image - torch.min(image))/(torch.max(image) - torch.min(image))
@@ -214,8 +302,10 @@ if __name__ == "__main__":
     # plt.imshow(best_image)
     # plt.axis('off')
     # plt.show()
-    mnist_dataloader = load_mnist()
-    cifar10_dataloader = load_cifar10()
+    mnist_mae_dataloader = load_mnist_for_mae()
+    mnist_dae_dataloader = load_mnist_for_dae()
+    cifar10_mae_dataloader = load_cifar10_for_mae()
+    cifar10_dae_dataloader = load_cifar10_for_dae()
 
     mae_mnist_model = load_mnist_mae()
     dae_mnist_model = load_mnist_dae()
@@ -223,12 +313,12 @@ if __name__ == "__main__":
     dae_cifar10_model = load_cifar10_dae()
 
 
-    # generate_mae_psnr_plot(mnist_dataloader, mae_model, 'mae_psnr_mnist.png')
-    generate_mae_psnr_plot(cifar10_dataloader, mae_cifar10_model, 'mae_psnr_cifar10.png')
-    # generate_dae_psnr_plot(mnist_dataloader, dae_model, 'dae_psnr_mnist.png')
-    generate_dae_psnr_plot(cifar10_dataloader, dae_cifar10_model, 'dae_psnr_cifar10.png')
+    # generate_mae_psnr_plot(mnist_mae_dataloader, mae_mnist_model, 'mae_psnr_mnist.png')
+    generate_mae_psnr_plot(cifar10_mae_dataloader, mae_cifar10_model, 'mae_psnr_cifar10.png')
+    # generate_dae_psnr_plot(mnist_dae_dataloader, dae_mnist_model, 'dae_psnr_mnist.png')
+    generate_dae_psnr_plot(cifar10_dae_dataloader, dae_cifar10_model, 'dae_psnr_cifar10.png')
 
-    # generate_mae_ssim_plot(mnist_dataloader, mae_model, 'mae_ssim_mnist.png', greyscale=True)
-    generate_mae_ssim_plot(cifar10_dataloader, mae_cifar10_model, 'mae_ssim_cifar10.png', greyscale=False)
-    # generate_dae_ssim_plot(mnist_dataloader, dae_model, 'dae_ssim_mnist.png', greyscale=True)
-    generate_dae_ssim_plot(cifar10_dataloader, dae_cifar10_model, 'dae_ssim_cifar10.png', greyscale=False)
+    # generate_mae_ssim_plot(mnist_mae_dataloader, mae_mnist_model, 'mae_ssim_mnist.png', greyscale=True)
+    generate_mae_ssim_plot(cifar10_mae_dataloader, mae_cifar10_model, 'mae_ssim_cifar10.png', greyscale=False)
+    # generate_dae_ssim_plot(mnist_dae_dataloader, dae_mnist_model, 'dae_ssim_mnist.png', greyscale=True)
+    generate_dae_ssim_plot(cifar10_dae_dataloader, dae_cifar10_model, 'dae_ssim_cifar10.png', greyscale=False)
